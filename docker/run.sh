9ac4c945ded2ca3314fda9ddfe2b8fc87e4d9fe3
Rene Meyer <rene.meyer@de.ibm.com>
2018-06-18 13:53:29 +0200

    restructure

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-# check for value of relative ingress path like this todo-web
-# without trailing slash
-echo "Ingresss path env var like / or /public/ " $INGRESSPATH
 # check for value of COS HTTPS URL like this https://s3.eu-de.objectstorage.softlayer.net/YOURBUCKET
 # without trailing slash
 echo "COS endpoint to static webfiles": $COS_ENDPOINT
@@ -14,7 +11,7 @@ echo "Bucket": $S3_BUCKET
 # S3 COS uri
 echo "COS_URI": $COS_URI
 
-if [ -z "$INGRESSPATH" ] || [ -z "$COS_ENDPOINT" ] || [ -z "$COS_URI" ] || [ -z "$S3_ACCESS_KEY" ] || [ -z "$S3_SECRET_KEY" ] || [ -z "$S3_BUCKET" ]; then
+if [ -z "$COS_ENDPOINT" ] || [ -z "$COS_URI" ] || [ -z "$S3_ACCESS_KEY" ] || [ -z "$S3_SECRET_KEY" ] || [ -z "$S3_BUCKET" ]; then
     echo "Variables not set exiting"
     exit;
 fi
@@ -28,4 +25,4 @@ sed -i "s,S3_BUCKET,${S3_BUCKET},g" /usr/local/openresty/nginx/conf/nginx.conf
 
 cat /usr/local/openresty/nginx/conf/nginx.conf
 
-nginx -g 'daemon off; error_log /dev/stderr debug;'
+nginx -g 'daemon off;'
